49700e95609af41716a4bf573f38fe038e003281
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-07-12 08:38:42 +0530

    Time: 8 ms (58.87%), Space: 44.8 MB (48.12%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+
+        int content = 0;
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0;
+        int j = 0;
+
+        while (i < g.size() && j < s.size()) {
+
+            if (s[j] >= g[i]) {
+
+                content++;
+                i++;
+                j++;
+
+            }
+
+            else {
+                j++;
+            }
+        }
+
+        return content;
+    }
+};
