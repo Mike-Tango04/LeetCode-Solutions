eb1b110849542c438de800443aff221dbcfa5abf
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-07-19 00:10:18 +0530

    Time: 0 ms (100%), Space: 9 MB (28.59%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+
+        int i = 0, j = s.size() - 1;
+
+        while(i <= j){
+            if(s[i] == s[j]){
+                return i;
+            }
+            i++ ;
+            j-- ;
+        }
+
+        return -1;
+        
+    }
+};
