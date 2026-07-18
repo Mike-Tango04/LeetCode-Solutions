dc68e10bc1caf02e27f90a4c579df8639e1d057f
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-07-18 22:01:51 +0530

    Time: 0 ms (100%), Space: 9.4 MB (66.05%) - LeetHub

---
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string rearrangeString(string s, char x, char y) {
+
+        string ans = "";
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == y){
+                ans += y;
+            }
+        }
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == x){
+                ans += x;
+            }
+        }
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != x && s[i] != y){
+                ans += s[i];
+            }
+        }
+
+        return ans;
+        
+    }
+};
