9e16c454963b09bfca816aca522a2596503763a1
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-07-21 11:30:00 +0530

    Time: 0 ms (100%), Space: 11.2 MB (83.25%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+
+        int maxSum = 0;
+        
+        int sum = 0;
+
+        for(int i = 0; i < accounts.size(); i++){
+
+            sum = 0;
+
+            for(int j = 0; j < accounts[i].size(); ++j){
+                
+                sum += accounts[i][j];
+
+            }
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum;
+        
+    }
+};
