c6209b96807b12cc98fb76bd7646e1e160f8d02e
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-07-31 01:08:53 +0530

    Time: 0 ms (100%), Space: 7.8 MB (18.95%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+
+        int start = 1;
+        int end = num / 2;
+
+        if(num == 1){
+            return true;
+        } 
+
+        while (start <= end) {
+
+            long long mid = start + (end - start) / 2;
+
+            if ((mid * mid) == num) {
+                return true;
+            } else if ((mid * mid) > num) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
