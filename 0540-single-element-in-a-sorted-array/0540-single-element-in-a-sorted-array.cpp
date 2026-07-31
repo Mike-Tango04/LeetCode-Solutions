3399c4c3c89afd4c5ba6a7e4b337406b74b270ad
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-08-01 01:47:07 +0530

    Time: 0 ms (100%), Space: 26.2 MB (69.6%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,24 +4,21 @@ public:
 
         int start = 0;
         int end = nums.size() - 1;
-        int mid = start + (end - start)/2;
 
         while(start < end){
 
-            if(mid % 2 == 1){
-                mid --;
+            int mid = start + (end - start)/2;
 
+            if(mid % 2 != 0){
+                mid --;
             }
 
             if(nums[mid] == nums[mid + 1]){
-                    start = mid + 2;
-                }
-                else{
-                    end = mid;
-                }
-
-            mid = start + (end - start)/2;
-            
+                start = mid + 2;
+            }
+            else{
+                end = mid;
+            }
         }
 
         return nums[start];
