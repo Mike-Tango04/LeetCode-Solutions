c64f6c5b864296426fdee135ae26e811e076636d
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-08-02 19:25:16 +0530

    Time: 0 ms (100%), Space: 15 MB (12.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -8,12 +8,9 @@ public:
 
         for(int i = 0; i < nums.size(); i++){
 
-            if(mp.find(target - nums[i]) != mp.end()){
-                v[0] = i;
-                v[1] = mp[target - nums[i]];
-            }
+            if(mp.find(target - nums[i]) != mp.end()) return {i, mp[target - nums[i]]};
             mp[nums[i]] = i;
         }
-        return v; 
+        return {};
     }
 };
