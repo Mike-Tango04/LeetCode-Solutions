b1ca32bfa1e4f70b69e4b62a76131c3860dc5e91
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-08-13 15:44:53 +0530

    Time: 0 ms (100%), Space: 9.1 MB (18.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,31 +2,26 @@ class Solution {
 public:
     bool isValid(string s) {
 
-        unordered_map<char, int> mp = {{'[', -1}, {'(', -2}, {'{', -3},
-                                       {']', 1},  {')', 2},  {'}', 3}};
+        unordered_map<int, int> mp = {{'[', -3}, {'{', -2}, {'(', -1},{']', 3}, {'}', 2}, {')', 1}};
 
         stack<char> st;
 
-        for (auto bracket : s) {
-
-            if (mp[bracket] < 0) {
-                st.push(bracket);
-            } else {
-                if (st.empty())
+        for(auto bracket : s){
+            if(mp[bracket] < 0) st.push(bracket);
+            else{
+                if(st.empty()){
                     return false;
-                else {
-                    char top = st.top();
-
-                    if (mp[top] + mp[bracket] != 0)
-                        return false;
+                }
+                else{
+                    char ch = st.top();
+                    if(mp[ch] + mp[bracket] != 0) return false;
                     st.pop();
                 }
             }
         }
 
-        if (st.empty())
-            return true;
-
-        return false;
+        if(!st.empty()) return false;
+        return true;
+        
     }
 };
