73aa104dffac35399b93b907d5ef5b9b6aff13a2
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-08-21 13:52:26 +0530

    Time: 3 ms (54.55%), Space: 11.4 MB (38.6%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+
+        string ans;
+
+        unordered_map<char, int> mp;
+
+        vector<pair<char, int>> v;
+
+        for(auto key : s) mp[key]++ ;
+
+        for(auto item : mp) v.push_back(item);
+
+        sort(v.begin(), v.end(), [] (auto &a, auto &b){
+            return a.second > b.second;
+        });
+
+        for(auto p : v){
+            ans.append(p.second, p.first);
+        }
+
+        return ans;
+        
+    }
+};
