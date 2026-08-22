2361825b1c01c81daf7e8fc049a7d70c91f32ff7
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-08-22 21:58:55 +0530

    Time: 0 ms (100%), Space: 62.9 MB (79.27%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+        int left = 0, right = height.size() - 1;
+        int area = 0, maxArea = 0;
+
+        while(left < right){
+
+            area = min(height[left], height[right]) * (right - left) ;
+
+            maxArea = max(maxArea, area);
+
+            if(height[left] < height[right]) left ++ ;
+            else right -- ;
+
+        }
+
+        return maxArea;
+        
+    }
+};
