ec4446b8ab3e22332c62a62913806f8ccc82e6c1
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-08-28 11:28:39 +0530

    Time: 0 ms (100%), Space: 8.7 MB (85.99%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+
+        unordered_map<char, int> mp;
+
+        vector<int> ans;
+
+        for (int i = 0; i < s.size(); i++) mp[s[i]] = i;
+        
+        int end = 0, start = 0;
+
+            for (int i = 0; i < s.size(); i++) {
+
+              end = max(end, mp[s[i]]);
+
+              if (i == end) {
+                ans.push_back(end - start + 1);
+                start = i + 1;
+            }
+
+        }
+        return ans;
+
+    }
+};
+
