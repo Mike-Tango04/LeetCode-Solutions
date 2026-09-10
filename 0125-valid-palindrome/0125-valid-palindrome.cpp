c1da6a88523d1f61968a7918368aa21ee863666c
MIKE TANGO <245231693+Mike-Tango04@users.noreply.github.com>
2026-09-10 17:59:31 +0530

    Time: 0 ms (100%), Space: 9.8 MB (93.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     bool isPalindrome(string s) {
 
-        int left = 0;
-        int right = s.size() - 1;
+        int left = 0, right = s.size() -1;
 
         while(left < right){
-              
-              while(left < right && !isalnum(s[left])) left++;
-              
-              while(left < right && !isalnum(s[right])) right-- ;
 
-              while( left < right && tolower(s[left]) != tolower(s[right])){
-                return false;
-              }
-              left++ ;
-              right-- ;
+            while(left < right && !isalnum(s[left])) left++ ;
+            while(left < right && !isalnum(s[right])) right-- ;
+            
+            if(left < right && tolower(s[left]) != tolower(s[right])) return false;
+            left++ ;
+            right-- ;
         }
-
-        return true;
         
+        return true;
     }
 };
